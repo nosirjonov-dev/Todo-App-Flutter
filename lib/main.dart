10f3370195dff3cf7c798bb97c4cc434008c5f65
Nosirjonov Qosimjon <nosirjonov.dev@gmail.com>
2025-11-30 16:30:48 +0500

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
-import 'add_task_page.dart';
+import 'pages/home_page.dart';
+import 'pages/add_task_page.dart';
 
 void main() {
   runApp(const MyTodoApp());
